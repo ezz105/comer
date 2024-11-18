6b206877b0846ff05d8846c8b3d2ac13ae3452b1
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-11-19 00:28:28 +0300

    feat :Implement User Login Functionality

--- backend/app/Http/Controllers/api/AuthController.php
+++ backend/app/Http/Controllers/api/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -17,6 +18,7 @@ class AuthController extends Controller
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|confirmed',
             'role_id' => 'required|string',
+            'phone_number' => 'required',
 
         ]);
 
@@ -27,7 +29,7 @@ class AuthController extends Controller
             ], 422);
         }
 
-        $credentials = $request->only('name', 'email', 'password', 'role_id');
+        $credentials = $request->only('name', 'email', 'password', 'role_id', 'phone_number');
 
         $user = User::create($credentials);
         $token = $user->createToken($user->name);
@@ -35,13 +37,41 @@ class AuthController extends Controller
 
         return [
             'user' => $user,
+            'role' => $user->role,
             'token' => $token->plainTextToken
         ];
     }
 
     public function login(Request $request)
     {
-        return "login";
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|exists:users',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors' => $validator->messages(),
+            ], 422);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
+
+        $token = $user->createToken($user->name);
+
+        return [
+            'user' => $user,
+            'role' => $user->role,
+            'token' => $token->plainTextToken
+        ];
+
     }
 
     public function logout(Request $request)

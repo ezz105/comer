fa6145417c070b37b30ae51b462f0daee8a36996
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2024-11-20 23:28:38 +0300

    feat : Adde pagination component

--- backend/app/View/Components/Pagination.php
+++ backend/app/View/Components/Pagination.php
@@ -1,25 +1,28 @@
 <?php
-
 namespace App\View\Components;
 
-use Closure;
-use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 class Pagination extends Component
 {
+    public $items;
+
     /**
      * Create a new component instance.
+     *
+     * @param \Illuminate\Pagination\LengthAwarePaginator $items
      */
-    public function __construct()
+    public function __construct($items)
     {
-        //
+        $this->items = $items;
     }
 
     /**
      * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
      */
-    public function render(): View|Closure|string
+    public function render()
     {
         return view('components.pagination');
     }

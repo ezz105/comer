fca98e5184f8b224f9c3ae3be0d4c54a82ad3a5a
waleed7333 <waleed733150@gmail.com>
2024-11-21 10:48:01 +0300

    fix: update layout.js file

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,17 +1,23 @@
-import localFont from "next/font/local";
-import "./globals.css";
+// src/app/layout.js
+
+import React from 'react'
+import './globals.css'
+import Navbar from '@/components/layout/Navbar'
+import Footer from '@/components/layout/Footer'
 
 export const metadata = {
-  title: "E-Commerec",
-  description: "Handcrafting Marketplace",
-};
+  title: 'E-Commerce - Handcrafted Goods Marketplace',
+  description: 'A marketplace to explore and purchase unique handcrafted items.',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="">
-        {children}
+      <body>
+        <Navbar />
+        <main className="min-h-[80vh]">{children}</main>
+        <Footer />
       </body>
     </html>
-  );
+  )
 }
